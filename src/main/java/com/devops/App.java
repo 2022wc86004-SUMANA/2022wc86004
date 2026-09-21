d89995c16e410ae0758acc272c365537d31d30de
2022wc86004-SUMANA <2022wc86004@wilp.bits-pilani.ac.in>
2026-09-21 17:20:26 +0530

    Update App.java

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -2,6 +2,6 @@ package com.devops;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from DevOps Jenkins!");
+        System.out.println("Hello from DevOps Jenkins! This is an example of Jenkins Continuous Integration");
     }
 }
